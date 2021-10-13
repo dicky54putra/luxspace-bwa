6d0fe2c0b459bcf35100509ad07187326b64e1b9
Dicky Saputra <saputradicky705@gmail.com>
2021-10-13 20:04:16 +0700

    useScrollAnchor useModalDom

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,26 @@
-import Clients from 'part/Clients'
-import Footer from 'part/Footer'
-import Header from 'part/Header'
-import Hero from 'part/Hero'
-import BrowseTheRoom from 'part/HomePage/BrowseTheRoom'
-import JustArrived from 'part/HomePage/JustArrived'
-import Sitemap from 'part/Sitemap'
-import React from 'react'
+import Clients from "part/Clients";
+import Footer from "part/Footer";
+import Header from "part/Header";
+import Hero from "part/HomePage/Hero";
+import BrowseTheRoom from "part/HomePage/BrowseTheRoom";
+import JustArrived from "part/HomePage/JustArrived";
+import Sitemap from "part/Sitemap";
+import React from "react";
+import useScrollAnchor from "helpers/hooks/useScrollAnchor";
+import useModalDom from "helpers/hooks/useModalDom";
 
 export default function HomePage() {
-    return (
-        <>
-            <Header theme="white" position="absolute" />
-            <Hero />
-            <BrowseTheRoom />
-            <JustArrived />
-            <Clients />
-            <Sitemap />
-            <Footer />
-        </>
-    )
+  useScrollAnchor();
+  useModalDom();
+  return (
+    <>
+      <Header theme="white" position="absolute" />
+      <Hero />
+      <BrowseTheRoom />
+      <JustArrived />
+      <Clients />
+      <Sitemap />
+      <Footer />
+    </>
+  );
 }
