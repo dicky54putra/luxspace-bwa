61d64cab5fb2089a71f888527ad0bd0ab6213c4b
Dicky Saputra <saputradicky705@gmail.com>
2022-01-08 18:05:24 +0700

    add conte4xt and animate add to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import 'assets/css/app.css';
-import Cart from 'pages/Cart';
-import Congratulation from 'pages/Congratulation';
-import HomePage from 'pages/HomePage';
-import NotFound from 'pages/NotFound';
-import ProductDetails from 'pages/ProductDetails';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
+import "assets/css/app.css";
+import Provider from "helpers/hooks/useGLobalContext";
+import Cart from "pages/Cart";
+import Congratulation from "pages/Congratulation";
+import HomePage from "pages/HomePage";
+import NotFound from "pages/NotFound";
+import ProductDetails from "pages/ProductDetails";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Provider>
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/categories/:idc" component={ProductDetails} />
-          <Route path="/categories/:idc/product/:idp" component={ProductDetails} />
+          <Route
+            path="/categories/:idc/product/:idp"
+            component={ProductDetails}
+          />
           <Route path="/cart" component={Cart} />
           <Route path="/congratulation" component={Congratulation} />
-
 
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
-    </div>
+    </Provider>
   );
 }
 
